1da4c1de820186a06a69bb2d46f6bea7ce45b7ed
gandzia44 <gamza.szymon@gmail.com>
2021-04-06 01:46:40 +0200

    Add project files.

---
+++ GameShop/MainWindow.xaml.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GameShop
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Numpad.Visibility = 0;
+        }
+
+        private void Grid_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if(e.LeftButton == MouseButtonState.Pressed)
+            {
+                DragMove();
+            }
+        }
+
+        public void Close_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Seller_Checked(object sender, RoutedEventArgs e)
+        {
+            if(Manager.IsChecked == true)
+            {
+                Manager.IsChecked = false;
+            }
+
+            if (Admin.IsChecked == true)
+            {
+                Admin.IsChecked = false;
+            }
+        }
+
+        private void Manager_Checked(object sender, RoutedEventArgs e)
+        {
+            if (Seller.IsChecked == true)
+            {
+                Seller.IsChecked = false;
+            }
+
+            if (Admin.IsChecked == true)
+            {
+                Admin.IsChecked = false;
+            }
+
+        }
+
+        private void Admin_Checked(object sender, RoutedEventArgs e)
+        {
+            if (Manager.IsChecked == true)
+            {
+                Manager.IsChecked = false;
+            }
+
+            if (Seller.IsChecked == true)
+            {
+                Seller.IsChecked = false;
+            }
+
+        }
+    }
+}
